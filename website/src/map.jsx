8f68ca0233befd0c6c6a620a34801ed79e9e6476
tablex1 <alexmain2024@gmail.com>
2025-02-08 13:10:15 +0000

    map can record click
    new click

--- website/src/map.jsx
+++ website/src/map.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 
 mapboxgl.accessToken = "pk.eyJ1IjoiYWxleG5lYWwyMDMwIiwiYSI6ImNtNncycWliNzBiMDAybHNkb3Fma3l1NmcifQ.mvN864hJb5SV2KW6yyYF8g"; // Replace with your token
@@ -6,6 +6,8 @@ mapboxgl.accessToken = "pk.eyJ1IjoiYWxleG5lYWwyMDMwIiwiYSI6ImNtNncycWliNzBiMDAyb
 const Map = () => {
   const mapContainerRef = useRef(null);
 
+  const [coordinates, setCoordinates] = useState([1,1]);
+  const [beginCoord, setBeginCoord] = useState([-0.5658080564214817,51.42583195427641])
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
@@ -16,14 +18,38 @@ const Map = () => {
     map.on("load", () => {
         map.resize();
     })
+    
+    map.on("click", (event) => {
+        event.preventDefault();
+        
+        setCoordinates([event.lngLat.lat,event.lngLat.lng]);
+        console.log([event.lngLat.lat,event.lngLat.lng]);
+        console.log(coordinates);
+        console.log("test");
+
+    })
+
+
+    const handleKeyDown = (event) => {
+        // Check if the spacebar (key code 32 or key ' ') is pressed
+        if (event.key === " ") {
+          setran("Spacebar was pressed!");
+          console.log(ran);
+          console.log("Spacebar was pressed!");
+        }
+      };
+    document.addEventListener("keydown", handleKeyDown);
     new mapboxgl.Marker()
       .setLngLat([-74.5, 40])
       .addTo(map);
 
     return () => map.remove(); // Cleanup on unmount
-  }, []);
+  }, [coordinates]);
 
-  return <div ref={mapContainerRef} className="map-container" />;
+  return( <div ref={mapContainerRef} className="map-container" />
+
+  )
+
 };
 
 export default Map;
